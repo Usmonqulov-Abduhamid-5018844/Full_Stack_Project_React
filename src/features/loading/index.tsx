4ce29633d270009bd39bd2cbca276b86a9137516
Abduhamid <karalevstvabitva@gmail.com>
2025-09-18 08:35:31 +0500

    Update

--- src/features/loading/index.tsx
+++ src/features/loading/index.tsx
@@ -1,29 +1,14 @@
 import { memo } from 'react';
+import Logo from "../../shared/assets/Logomark.svg"
 
 const LoadingDashbord = () => {
   return (
-      <div className="w-[250px] h-screen bg-slate-900 sticky top-0 left-0 p-4 text-white">
-      <div className=" flex gap-4 justify-center mt-3">
-        <h2 className="text-3xl text-center ml-14">Medic</h2>
-      </div>
-      <ul className="my-10 flex flex-col gap-2">
-        <li className='block p-2 mb-2 rounded-md bg-white h-[35px] text-slate-900'>
-        </li>
-        <li className='block p-2 mb-2 rounded-md bg-white h-[35px] text-slate-900'>
-        </li>
-        <li className='block p-2 mb-2 rounded-md bg-white h-[35px] text-slate-900'>
-        </li>
-        <li className='block p-2 mb-2 rounded-md bg-white h-[35px] text-slate-900'>
-        </li>
-        <li className='block p-2 mb-2 rounded-md bg-white h-[35px] text-slate-900'>
-        </li>
-        <li className='block p-2 mb-2 rounded-md bg-white h-[35px] text-slate-900'>
-        </li>
-        <li className='block p-2 mb-2 rounded-md bg-white h-[35px] text-slate-900'>
-        </li>
+     <div className="flex items-center justify-center h-screen">
+  <div className="w-[100px] h-[100px]">
+    <img className='w-40' src={Logo} alt="" />
+  </div>
+</div>
 
-      </ul>
-    </div>
   );
 };
 
