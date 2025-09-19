1f45f7fa8fa3e5095d6f50278f6c644cde718155
Abduhamid <karalevstvabitva@gmail.com>
2025-09-19 23:28:32 +0500

    U

--- src/features/statistic/pages/Statistic.tsx
+++ src/features/statistic/pages/Statistic.tsx
@@ -1,11 +1,22 @@
-import { memo } from 'react';
-
+import { memo } from "react";
+import { Chart } from "../components/chart";
+import { Product } from "../components/product";
+import { Users } from "../components/users";
+import { Liler_Chart } from "../components/liner_chart";
 
 const Statistic = () => {
   return (
-    <div className="Statistic">
-      <h2>Statistic</h2>
+    <>
+    <div className="w-[500px] mt-10 ml-6 flex ">
+      <Chart />
+
+      <Product />
+      <Users/>
     </div>
+    <div className="mt-10 w-[90%] mx-auto">
+      <Liler_Chart/>
+    </div>
+    </>
   );
 };
 
